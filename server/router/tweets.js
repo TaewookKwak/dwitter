58253dab761673b4aa014e2e436dda80c376b7da
Taewook <rhkrxodnr@gmail.com>
2022-03-10 23:06:58 +0900

    [2022.03.10] MVC, VALIDATION

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,84 +1,33 @@
 import express from 'express'
 import 'express-async-errors'
+import * as tweetController from '../controller/tweets.js'
+import { body, param } from 'express-validator'
+import { validate } from '../middleware/validator.js'
 
 const router = express.Router()
-let tweets = [
-  {
-    id: '1',
-    text: '드림코딩에서 강의 들으면 너무 좋으다',
-    createdAt: Date.now().toString(),
-    name: 'Bob',
-    username: 'bob',
-    url:
-      'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
-  {
-    id: '2',
-    text: '드림코딩에서 강의 들으면 너무 좋으다',
-    createdAt: Date.now().toString(),
-    name: 'Ellie',
-    username: 'ellie',
-    url:
-      'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
-  {
-    id: '3',
-    text: '드림코딩에서 강의 들으면 너무 좋으다',
-    createdAt: Date.now().toString(),
-    name: 'Sam',
-    username: 'sam',
-    url:
-      'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
+const vailidateTweet = [
+  body('text')
+    .notEmpty()
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('text should be at least 3 chars'),
+  validate,
 ]
+// GET /tweets
+// GET /tweets?username=:username
+router.get('/', tweetController.getTweets)
 
-router.get('/', (req, res, next) => {
-  const username = req.query.username
-  const data = username ? tweets.filter((t) => t.username === username) : tweets
-  res.status(201).json(data)
-})
+// GET /tweets/:id
+router.get('/:id', tweetController.getTweet)
 
-router.get('/:id', (req, res, next) => {
-  const id = req.params.id
-  const tweet = tweets.find((t) => t.id === id)
-  if (tweet) {
-    res.status(200).json(tweet)
-  } else {
-    res.status(404).json({ message: `Tweet id ${id} not found` })
-  }
-})
+// POST /tweeets
+router.post('/', vailidateTweet, tweetController.createTweet)
 
-router.post('/', (req, res, next) => {
-  const { text, username, name } = req.body
-  const tweet = {
-    id: Date.now().toString(),
-    text,
-    createdAt: new Date(),
-    name,
-    username,
-  }
-  tweets = [tweet, ...tweets]
-  res.status(201).json(tweet)
-})
+// PUT /tweets/:id
+router.put('/:id', vailidateTweet, tweetController.updateTweet)
 
-router.put('/:id', (req, res, next) => {
-  const id = req.params.id
-  const text = req.body.text
-  const tweet = tweets.find((t) => t.id === id)
-  if (tweet) {
-    tweet.text = text
-    res.status(200).json(tweet)
-  } else {
-    res.status(404).json({ message: `Tweet id ${id} not found` })
-  }
-})
-
-router.delete('/:id', (req, res, next) => {
-  const id = req.params.id
-  tweets = tweets.filter((t) => t.id !== id)
-  res.sendStatus(204)
-  res.status(201).send('DELETE: /')
-})
+// DELETE /tweets/:id
+router.delete('/:id', tweetController.deleteTweet)
 
 export default router
 
